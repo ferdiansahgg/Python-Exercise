b1aaefb45f3cbb791d27a23d6368a97d68f4f061
ferdiansahgg <ferdiayana@gmail.com>
2019-12-20 13:43:01 +0700

    adding

---
+++ Dictionaries.py
@@ -0,0 +1,16 @@
+monthConversions = {
+    "Jan": "January",
+    "Feb": "February",
+    "Mar": "March",
+    "Apr": "April",
+    "May": "May",
+    "Jun": "Juni",
+    "Jul": "July",
+    "Aug": "August",
+    "Sep": "September",
+    "Oct": "October",
+    "Nov": "November",
+    "Dec": "December",
+}
+
+print(monthConversions.get("Dec"))
--- ifstatments.py
+++ ifstatments.py
@@ -1,4 +1,24 @@
 """Saya bangun
-jika saya lapar
-    saya makan nasi uduk"""
+jika saya lapar:
+    saya makan nasi uduk
+jika tidak, jika saya tidak membeli sarapan:
+    saya berangkat kerja
+jika tidak,
+    saya mandi"""
+
+laki_laki = True
+tinggi = False
+if laki_laki or tinggi: #and
+    print("Kamu Adalah laki-laki atau kamu tinggi")
+elif laki_laki and not(tinggi):
+    print("Kamu adalah laki-laki yang pendek")
+elif not(laki_laki) and tinggi:
+    print("Kamu adalah laki-laki yang pendek")
+else:
+    print("Kamu bukan laki-laki atau tidak tinggi")
+
+
+
+
+
 
